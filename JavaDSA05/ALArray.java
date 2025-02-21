1a0a547284c544e61b3d854110e46bbdbb8b970f
devtushar001 <tc341269@gmail.com>
2025-02-21 01:33:04 -0800

    poirt

---
+++ JavaDSA05/ALArray.java
@@ -0,0 +1,24 @@
+package JavaDSA05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ALArray {
+  public static void main(String[] args) {
+    // Array List Java
+
+    ArrayList<Integer> ArrList = new ArrayList<>();
+    // List<Integer> ArrList = new ArrayList<>();
+    // Integer is generic class every inbuild data structure in java collection are all inbuild data structure in java collection can printed data structure
+    // generics
+
+    // class<E> generice
+
+    ArrList.add(1);
+    ArrList.add(3);
+    ArrList.add(4);
+
+    System.out.println(ArrList);
+
+  }
+}
