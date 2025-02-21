94752ea04a3a75508cd3c3f2f167856c2d57b526
devtushar001 <tc341269@gmail.com>
2025-02-21 01:52:18 -0800

    Array List creation and method

--- JavaDSA05/ALArray.java
+++ JavaDSA05/ALArray.java
@@ -9,7 +9,8 @@ public class ALArray {
 
     ArrayList<Integer> ArrList = new ArrayList<>();
     // List<Integer> ArrList = new ArrayList<>();
-    // Integer is generic class every inbuild data structure in java collection are all inbuild data structure in java collection can printed data structure
+    // Integer is generic class every inbuild data structure in java collection are
+    // all inbuild data structure in java collection can printed data structure
     // generics
 
     // class<E> generice
@@ -18,7 +19,23 @@ public class ALArray {
     ArrList.add(3);
     ArrList.add(4);
 
+    System.out.println(ArrList); // printing array with stored data
+    System.out.println(ArrList.size()); // checking size
+
+    ArrList.add(2, 5);// index, value (example of method overloading// number of parameter or change
+                      // type of parameter)
+    System.out.println(ArrList);
+    // System.out.println(ArrList[i]); //error
+
+    System.out.println(ArrList.get(3));
+    ArrList.add(0, 10);
     System.out.println(ArrList);
 
+    ArrList.remove(3); //Pass index
+
+    System.out.println(ArrList.remove(3)); //4
+
+    // add(index, value) get(index) size() remove(index)
+    
   }
 }
